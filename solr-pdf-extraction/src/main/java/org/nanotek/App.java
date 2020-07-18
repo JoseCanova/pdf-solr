9f36a4faeab0a9bfc7e2a5f53dab478c465885db
Jose Canova <jose.c.canova@gmail.com>
2020-07-18 11:56:37 -0300

    firstcommit

---
+++ solr-pdf-extraction/src/main/java/org/nanotek/App.java
@@ -0,0 +1,15 @@
+package org.nanotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public App() {
+	}
+
+	public static void main(String[] args) { 
+		SpringApplication.run(App.class, args);
+	}
+}
